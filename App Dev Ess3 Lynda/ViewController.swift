029e33cfddf256e486539012ed5cf06e16b383e2
yury-g <yury.gitman@gmail.com>
2016-10-26 11:54:52 -0400

    Initial Commit

---
+++ App Dev Ess3 Lynda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App Dev Ess3 Lynda
+//
+//  Created by Yury Gitman on 10/26/16.
+//  Copyright © 2016 com.yuryg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
